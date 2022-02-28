89888e945665b6343af9d7fea3e370da21a71f1b
sagar-pardhi <sgrpardhi007@gmail.com>
2022-02-28 15:34:08 +0530

    added episode model and updated admin

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Episode
 
-# Register your models here.
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ("podcast_name", "title", "pub_date")
