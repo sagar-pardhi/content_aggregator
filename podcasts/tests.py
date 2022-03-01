beb5fe10433e70a33398dfd221fa32ae3962e652
sagar-pardhi <sgrpardhi007@gmail.com>
2022-03-01 20:08:35 +0530

    added test for homepage

--- podcasts/tests.py
+++ podcasts/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from .models import Episode
+from django.urls.base import reverse
 
 
 class PodCastsTests(TestCase):
@@ -24,3 +25,15 @@ class PodCastsTests(TestCase):
         self.assertEqual(
             str(self.episode), "My Python Podcast: My Awesome Podcast Episode"
         )
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get(reverse("homepage"))
+        self.assertTemplateUsed(response, "homepage.html")
+
+    def test_homepage_list_contents(self):
+        response = self.client.get(reverse("homepage"))
+        self.assertContains(response, "My Awesome Podcast Episode")
