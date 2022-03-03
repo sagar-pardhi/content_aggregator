a758f87c1fb0f57e5fb8ae449fef040f035b8d91
sagar-pardhi <sgrpardhi007@gmail.com>
2022-03-03 16:01:55 +0530

    updated startjobs command

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -6,21 +6,44 @@ from dateutil import parser
 from podcasts.models import Episode
 
 
-class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
-        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
+def save_new_episodes(feed):
+    """Saves new episodes to the database.
+    Checks the episode GUID against the episodes currently stored in the
+    database. If not found, then a new `Episode` is added to the database.
 
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid).exists():
-                episode = Episode(
-                    title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid,
-                )
-                episode.save()
+    Args:
+        feed: requires a feedparser object
+    """
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
+
+
+def fetch_realpython_episodes():
+    """Fetches new episodes from RSS for The Real Python Podcast."""
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(_feed)
+
+
+def fetch_talkpython_episodes():
+    """Fetches new episodes from RSS for the Talk Python to Me Podcast."""
+    _feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+    save_new_episodes(_feed)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        fetch_realpython_episodes()
+        fetch_talkpython_episodes()
