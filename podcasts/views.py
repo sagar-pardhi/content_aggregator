6c85691159a7920223ffdb1a062fa13910ac4a60
sagar-pardhi <sgrpardhi007@gmail.com>
2022-03-08 20:03:59 +0530

    added pagination

--- podcasts/views.py
+++ podcasts/views.py
@@ -5,6 +5,7 @@ from .models import Episode
 class HomePageView(ListView):
     template_name = "homepage.html"
     model = Episode
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
